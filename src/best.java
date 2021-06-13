1ff769c91a011a33d29348aa7fb425620dfed579
W-ay <1012370241@qq.com>
2021-06-13 22:43:56 +0800

    井字棋
    修改了必须下满棋盘才能结束游戏的bug
    游戏基本功能已实现

--- src/best.java
+++ src/best.java
@@ -2,22 +2,20 @@ import java.util.Scanner;
 
 public class best {
 	public static void main(String args[]) {
-		var chess = new game();//定义棋盘大小
-		chess.csh();//初始化棋盘
-		chess.drawFirst();//首次绘制
+		var chess = new game();
+		chess.start();//初始化游戏
 		var in = new Scanner(System.in);
-		for(int i = 0;i<9;i++) {//下棋过程
-		int x = in.nextInt()- 1;
-		int y = in.nextInt()- 1;
-		if((x>0&&x<4)&&(y>0&&y<4))//判断输入是否正确
-			{
-			if(chess.draw(x,y) != true)//下棋不和规则，则重新下棋
-				i--; 
+		while(true) {
+			int x = in.nextInt()- 1;
+			int y = in.nextInt()- 1;
+			if(chess.draw1(x, y) == true) {
+				chess.draw();
 			}
-		else {
-			System.out.println("输入的位置不对，请重新输入");
-			i--;
-		}
+			if(chess.judgeWin()==true) {
+				System.out.println("游戏结束！");
+				System.out.println("恭喜:"+chess.returnChess()+"获得游戏胜利！");
+				break;
+			}
 		}
 	}
 }
--- src/game.java
+++ src/game.java
@@ -2,64 +2,67 @@
 public class game {
 	private int a = 3;//棋盘横向多大
 	private int b = 3;//棋盘竖向多大
-	private int number = 1;
-//	game(int a, int b) {
-//		this.a = a;
-//		this.b = b;
-//		}
+	private int number = 1;//记录当前是哪个棋子
 	char [][]win = new char[a][b];
 	
-	public void csh() {//初始化
-		for(int i = 0;i<a;i++) {
+	public void start() {
+		for(int i = 0;i<a;i++) {//初始化棋盘数组
 			for(int j = 0; j<b;j++) {
 				win[i][j] = '#';
 			}
 		}
+		this.draw();//输出当前棋盘
 	}
-	public void drawFirst() {
-		for(char[] a: win) { //输出当前棋盘
+	public boolean draw1(int x,int y) {
+		if(judgeRule(x,y) == true) {
+			if(number%2 == 0) //O先；X后;number为奇数则是O,偶数则是X;
+				win[x][y] = 'X';
+			else 
+				win[x][y] = 'O';
+		number++;
+		draw();
+		}
+		return false;
+	}
+	public void draw() {//输出当前棋盘
+		for(char[] a: win) { 
 			System.out.println(a);
 		}
 	}
-	public boolean draw(int x,int y) {
-		if(x<a && y<b ) //判断是否在棋盘内下棋
+	public boolean judgeRule(int x,int y) {//判断下棋是否合规矩
+		
+		if((x>=0&&x<a)&&(y>=0&&y<b)) //判断是否在棋盘内下棋
 		{
-			if(win[x][y] == '#') //判断是否此处已有棋子
-			{
-				if(number%2 == 0) //O先；X后
-					win[x][y] = 'X';
-				else 
-					win[x][y] = '0';
-				if(this.judge()==true)
-					System.out.println(win[x][y]+"赢得了比赛");
-				number++;
+			if(win[x][y] == '#') { //判断是否此处已有棋子
+				return true;
 			}
 			else {
-				System.out.println("此处已有棋子，请重新下棋");
+				System.out.println("此处已有棋子，请重新下棋");draw();
 				return false;
 			}
 		}
 		else {
-			System.out.println("棋子超出了棋盘");
+			System.out.println("棋子超出了棋盘");draw();
 			return false;
 		}
-		for(char[] a: win) { //下完棋后输出当前棋盘
-			System.out.println(a);
-		}
-		return true;
 	}
-	public boolean judge() {
+	public char returnChess() {//返回当前是哪个棋子
+		if(number%2==0)
+			return 'O';
+		else return 'X';
+	}
+	public boolean judgeWin() {//判断是否获胜
+		
 		for(int i = 0;i<3;i++) {
-			if(win[i][0]==win[i][1]&&win[i][1]==win[i][2])
+			if(win[i][0]==win[i][1]&&win[i][1]==win[i][2]&&win[i][0]!='#')//检查横向
 				return true;
-			else if(win[0][i]==win[1][i]&&win[1][i]==win[2][i])
+			else if(win[0][i]==win[1][i]&&win[1][i]==win[2][i]&&win[0][i]!='#')//检查竖向
 				return true;
 		}
-		if(win[0][0]==win[1][1]&&win[1][1]==win[2][2])//判断斜向是否赢棋
+		if(win[0][0]==win[1][1]&&win[1][1]==win[2][2]&&win[0][0]!='#')//判断斜向1是否赢棋
 			return true;
-		else if(win[0][2]==win[1][1]&&win[1][1]==win[2][0])
+		else if(win[0][2]==win[1][1]&&win[1][1]==win[2][0]&&win[0][2]!='#')//检查斜向2
 			return true;
-	//	System.out.println("判断程序未正确运行！");
 		return false;
 	}
 }
